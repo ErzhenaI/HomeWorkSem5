f7e2c45d82b112ced3dc991d96f4a849fe2b8206
Erzhena Istomina <era155@mail.ru>
2023-06-17 14:36:49 +0800

    done Tasks 34_36_38

---
+++ Task36/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-30, 31); 
+}
+void SearchSum(int[]array)
+{
+    int Sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    Sum  += array[i]; 
+    Console.WriteLine($"Сумма равна: {Sum}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[]array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+SearchSum (array);
---
+++ Task38/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputArray(double[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2);
+}
+double MinArray(double[] array)
+{
+    double min = array[0];
+    foreach (double a in array)
+        if(min > a) min = a;
+    return min;
+}
+double MaxArray(double[] array)
+{
+    double max = array[0];
+    foreach (double b in array)
+        if(max < b) max = b;
+    return max;
+}
+double Result(double min, double max)
+{
+    double result = max - min;
+    return result;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+double min = MinArray(array);                              
+double max = MaxArray(array);                              
+double res = Result(min, max);
+Console.WriteLine($"Результат: {res}");
